f61d5ef9dc556a926420c0962a8a570d06193e56
Pouria <82539162+Pouria1378@users.noreply.github.com>
2024-01-30 01:24:02 +0330

    test pm2 :)

--- app/i18n/client.js
+++ app/i18n/client.js
@@ -38,7 +38,6 @@ export function useTranslation(lng, ns, options) {
             if (activeLng === i18n.resolvedLanguage) return
             setActiveLng(i18n.resolvedLanguage)
         }, [activeLng, i18n.resolvedLanguage])
-
         // eslint-disable-next-line react-hooks/rules-of-hooks
         useEffect(() => {
             if (!lng || i18n.resolvedLanguage === lng) return
